f7ea8e51f983dba20f22d1ee5796fed6430f4268
Opilane TTHK <opilane@tthk.ee>
2021-11-23 13:30:37 +0200

    first commit

---
+++ loginvalidation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace loginvalidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string login = "adming";
+            string password = "pass1234";
+
+            string userName, userPassword;
+
+            Console.WriteLine("enter your password:");
+            userName = Console.ReadLine();
+            Console.WriteLine("enter your password:");
+            userPassword = Console.ReadLine();
+
+            if (login == userName && password == userPassword);
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Oops. Something is wrong);
+            }
+                
+
+
+        }
+    }
+}
